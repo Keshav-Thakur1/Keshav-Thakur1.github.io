87021bfea4e85884f27a0e55b7904871d47d115b
Keshav-Thakur1 <141169280+Keshav-Thakur1@users.noreply.github.com>
2023-08-04 19:18:22 +0530

    Update form.php

--- form.php
+++ form.php
@@ -5,11 +5,10 @@ $message= $_POST['message'];
 $to = "keshavthakur5103@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@https://keshav-thakur1.github.io/" . "\r\n" .
+$headers = "From: noreply@keshav-thakur1.github.io" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
-//redirect
 header("Location:thanku.html");
 ?>
